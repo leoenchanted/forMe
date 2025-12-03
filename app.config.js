1420ba85c9ed55025832ab201165d8583729d1ea
liao13 <1198067041@qq.com>
2025-12-03 20:38:44 +0800

    Allow dev and prod app co-exist

---
+++ app.config.js
@@ -0,0 +1,51 @@
+const IS_DEV = process.env.APP_VARIANT === 'development';
+const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+
+export default {
+  expo: {
+    name: IS_DEV ? "forme (Dev)" : "forme", // 开发版名字不一样
+    slug: "forme",
+    version: "1.1.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      "image": "./assets/splash.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#ffffff"
+    },
+    assetBundlePatterns: [
+      "**/*"
+    ],
+    ios: {
+      "supportsTablet": true,
+      "bundleIdentifier": IS_DEV ? "com.yourname.forme.dev" : "com.yourname.forme"
+    },
+    android: {
+      "adaptiveIcon": {
+        "foregroundImage": "./assets/icon.png",
+        "backgroundColor": "#ffffff"
+      },
+      // 核心修改：如果是开发版，包名后面加 .dev，这样就能共存了！
+      "package": IS_DEV ? "com.yourname.forme.dev" : "com.yourname.forMe"
+    },
+    web: {
+      "favicon": "./assets/favicon.png"
+    },
+    plugins: [
+      [
+        "expo-media-library",
+        {
+          "photosPermission": "Allow forme to save wallpapers to your photos.",
+          "savePhotosPermission": "Allow forme to save wallpapers to your photos.",
+          "isAccessMediaLocationEnabled": true
+        }
+      ]
+    ],
+    extra: {
+      eas: {
+        projectId: "e3327d5b-e82a-4111-950f-affc5497935c" // ⚠️ 如果你之前删了这一行，这里不填也没事，EAS会自动识别
+      }
+    }
+  }
+};
